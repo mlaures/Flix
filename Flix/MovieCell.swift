e3e76f0a26f9a07c6656cf1ca0cd1ee5e79e52fb
Mei-Ling Laures <mlaures@fb.com>
2017-06-21 16:24:00 -0700

    added icons for app, UX in app

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Mei-Ling Laures on 6/21/17.
+//  Copyright © 2017 Mei-Ling Laures. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
