8c70b067391b9c329bbc2ed65d16034549ef3076
Mei-Ling Laures <mlaures@fb.com>
2017-06-23 15:53:05 -0700

    added infinite scrolling to poster view

--- Flix/PosterViewController.swift
+++ Flix/PosterViewController.swift
@@ -21,6 +21,10 @@ class PosterViewController: UIViewController, UICollectionViewDataSource, UIColl
     
     var refreshControl: UIRefreshControl!
     
+    // for infinite scrolling - dont load more data when uneeded and which page to load
+    var loadingData = false
+    var pageCount = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -45,14 +49,16 @@ class PosterViewController: UIViewController, UICollectionViewDataSource, UIColl
     
     // calls the fetch function whenever the refresh control is called
     func didPullToRefresh(_ refreshControl: UIRefreshControl) {
-//        loadingData = true
-//        pageCount = 1
+        loadingData = true
+        // reset to the beginning! get the first page and remove all currently loaded movies
+        pageCount = 1
+        movies = []
         fetchSuperheros()
     }
     
     func fetchSuperheros() {
         // This is the networking request
-        let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US")!
+        let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US&page=\(pageCount)")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         let task = session.dataTask(with: request) { (data, response, error) in
@@ -65,16 +71,20 @@ class PosterViewController: UIViewController, UICollectionViewDataSource, UIColl
                 
                 // put the wanted data (the movies) into an array of dictionaries
                 let movies = dataDictionary["results"] as! [[String:Any]]
-                self.movies = movies
+                self.movies += movies
                 
                 // reload the table so that it displays the information from the networking request
                 self.posterCollection.reloadData()
+                
                 
                 // the network request finished so stop loading
                 self.activityIndicator.stopAnimating()
                 
                 // the network has finished fetching data, so if table is refreshing, end the loading signal
                 self.refreshControl.endRefreshing()
+                
+                // data is finished fetching
+                self.loadingData = false
                 
             }
         }
@@ -95,19 +105,21 @@ class PosterViewController: UIViewController, UICollectionViewDataSource, UIColl
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         // get the cell that is going to be filled
         let cell = posterCollection.dequeueReusableCell(withReuseIdentifier: "PosterCell", for: indexPath) as! PosterCell
-        let movie = movies[indexPath.row]
+        if !movies.isEmpty {
+            let movie = movies[indexPath.row]
         
-        let base = "https://image.tmdb.org/t/p/w500"
+            let base = "https://image.tmdb.org/t/p/w500"
         
-        // store and display poster in cell
-        if let path = movie["poster_path"] as? String {
-            let posterURL = URL(string: base + path)
+            // store and display poster in cell
+            if let path = movie["poster_path"] as? String {
+                let posterURL = URL(string: base + path)
             
-            // set the elements in the cell to be what needs to be displayed
-            cell.posterImage.af_setImage(withURL: posterURL!)
+                // set the elements in the cell to be what needs to be displayed
+                cell.posterImage.af_setImage(withURL: posterURL!)
             
-        } else {
-            cell.posterImage.image = nil
+            } else {
+                cell.posterImage.image = nil
+            }
         }
         
         
@@ -124,6 +136,22 @@ class PosterViewController: UIViewController, UICollectionViewDataSource, UIColl
         // pass the correct information from the list of movies to the next view
         let movie = movies[index.row]
         control.movie = movie
+    }
+    
+    // function to load more movie data
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        // do not already want to be loading the data
+        if !loadingData {
+            let tableHeight = posterCollection.contentSize.height
+            let scrollThreshold = tableHeight - posterCollection.bounds.size.height
+            // should be a certain distance from the bottom of the list of movies before more data is loaded
+            if scrollView.contentOffset.y > scrollThreshold && posterCollection.isDragging {
+                loadingData = true
+                activityIndicator.startAnimating()
+                pageCount += 1
+                fetchSuperheros()
+            }
+        }
     }
     
     /*
